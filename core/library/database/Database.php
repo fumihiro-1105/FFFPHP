15a6d5b2fa454d379d7f975b58bea7dc0c5cdbbd
okumura <fumihiro.1105@gmail.com>
2014-06-22 01:07:22 +0900

    #2 Database への接続、Database からの切断

---
+++ core/library/database/Database.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Created By Fumihiro
+ * Created At 2014/06/20 23:25
+ */
+
+namespace core\library\database;
+
+require_once __DIR__ . '/../../vendor/spyc.php';
+
+class Database
+{
+    const CONNECTION_MASTER = 'master';
+
+    const CONNECTION_NAME_DEFAULT = 'default';
+
+    /**
+     * @var array $setting
+     */
+    private $setting;
+
+    /**
+     * @var string $connectionName
+     */
+    private $connectionName;
+
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var string
+     */
+    private $database;
+
+    /**
+     * @var string
+     */
+    private $user;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var bool
+     */
+    private $statusOpen = false;
+
+    /**
+     * @var \Mysqli
+     */
+    private $mysql;
+
+    /**
+     * @param string $connectionName
+     */
+    public function __construct($connectionName = self::CONNECTION_NAME_DEFAULT)
+    {
+
+        $this->connectionName = $connectionName;
+
+        $this->readConfig();
+
+        $this->initialize();
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    public function open()
+    {
+
+        if (!$this->isOpen()) {
+            $this->mysql      = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+            $this->statusOpen = true;
+
+        }
+
+        return $this->statusOpen;
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function close()
+    {
+
+        $result = true;
+
+        if ($this->mysql) {
+            $result           = $this->mysql->close();
+            $this->statusOpen = false;
+
+            $this->mysql = null;
+
+        }
+
+        return $result;
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOpen()
+    {
+        return $this->statusOpen;
+    }
+
+    /**
+     * 設定情報（yml ファイル）を取り込む
+     */
+    private function readConfig()
+    {
+
+        $this->setting = spyc_load_file(__DIR__ . '/../../config/database.yml');
+
+    }
+
+    /**
+     * 初期化
+     */
+    private function initialize()
+    {
+
+        $this->checkConnection();
+
+        $this->checkConnectionConfig();
+
+        $this->setConnectionParameter();
+
+    }
+
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    private function checkConnection()
+    {
+
+        if (!isset($this->setting[self::CONNECTION_MASTER])) {
+            throw new \Exception('[Connection] Not found master setting.');
+        }
+
+        if (!isset($this->setting[self::CONNECTION_MASTER][$this->connectionName])) {
+            throw new \Exception('[Connection] Not found "' . $this->connectionName . '" setting.');
+        }
+
+        return true;
+
+    }
+
+    /**
+     * @return bool
+     * @throws \Exception
+     */
+    private function checkConnectionConfig()
+    {
+
+        $params = array(
+            'host',
+            'database',
+            'user',
+        );
+
+        $connection = $this->setting[self::CONNECTION_MASTER][$this->connectionName];
+
+        foreach ($params as $param) {
+            if (!isset($connection[$param])) {
+                throw new \Exception('[Connection Config] Not found "' . $param . '" parameter.');
+            }
+
+            if ($connection[$param] === null || $connection[$param] === '') {
+                throw new \Exception('[Connection Config] Null "' . $param . '" parameter.');
+            }
+        }
+
+        if (!is_string($connection['host'])) {
+            throw new \Exception('[Connection Config] Not string "host".');
+        }
+
+        if (!is_string($connection['database'])) {
+            throw new \Exception('[Connection Config] Not string "database".');
+        }
+
+        if (!is_string($connection['user'])) {
+            throw new \Exception('[Connection Config] Not string "user".');
+        }
+
+        if (isset($connection['password']) && !is_string($connection['password'])) {
+            throw new \Exception('[Connection Config] Not numeric "password".');
+        }
+
+        return true;
+
+    }
+
+    /**
+     *
+     */
+    private function setConnectionParameter()
+    {
+
+        $connection = $this->setting[self::CONNECTION_MASTER][$this->connectionName];
+
+        foreach ($connection as $key => $value) {
+            $connection[$key] = $value === '' ? null : $value;
+        }
+
+        $this->host     = $connection['host'];
+        $this->database = $connection['database'];
+        $this->user     = $connection['user'];
+        $this->password = isset($connection['password']) ? $connection['password'] : '';
+
+    }
+
+}
---
+++ core/test/database/DatabaseTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created By Fumihiro
+ * Created At 2014/06/20 23:26
+ */
+
+namespace core\test\database;
+
+use core\library\database\Database;
+
+require_once __DIR__ . '/../../library/database/Database.php';
+
+class DbTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+    }
+
+    /**
+     * セッティング情報（yml ファイル）が正しく読み込まれているかをテスト
+     */
+    public function testSetting()
+    {
+        $db = new Database();
+        $this->assertEquals('127.0.0.1', $db->getHost());
+        $this->assertEquals('msr', $db->getDatabase());
+        $this->assertEquals('root', $db->getUser());
+        $this->assertEquals('', $db->getPassword());
+    }
+
+    /**
+     * セッティング情報（yml ファイル）が正しく読み込まれているかをテスト
+     * コネクション切り替え
+     */
+    public function testSettingOtherConnection()
+    {
+        $db = new Database('platform');
+        $this->assertEquals('db.platform.local', $db->getHost());
+        $this->assertEquals('msr_platform', $db->getDatabase());
+        $this->assertEquals('root', $db->getUser());
+        $this->assertEquals('', $db->getPassword());
+    }
+
+    /**
+     * 接続のテスト
+     */
+    public function testDatabaseOpen()
+    {
+        $db = new Database();
+        $this->assertTrue($db->open());
+        $this->assertTrue($db->isOpen());
+    }
+
+    /**
+     * 切断のテスト
+     */
+    public function testDatabaseClose()
+    {
+        $db = new Database();
+        $this->assertTrue($db->open());
+        $this->assertTrue($db->Close());
+        $this->assertFalse($db->isOpen());
+    }
+
+    /**
+     * 接続せずに切断をした場合
+     */
+    public function testDatabaseCloseNotOpen()
+    {
+        $db = new Database();
+        $this->assertTrue($db->close());
+        $this->assertFalse($db->isOpen());
+    }
+
+    /**
+     * 切断を2回以上連続で行った場合
+     */
+    public function testDatabaseCloseAndClose()
+    {
+        $db = new Database();
+        $this->assertTrue($db->open());
+        $this->assertTrue($db->close());
+        $this->assertTrue($db->close());
+        $this->assertFalse($db->isOpen());
+    }
+
+}
